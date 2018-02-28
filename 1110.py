a8c42bbd79e7528f0157b5c83dff6c6784306237
Your Name <helton.costa@tvglobo.com.br>
2018-02-28 02:29:09 -0300

    solução do problema 1110

--- 1110.py
+++ 1110.py
@@ -1,37 +1,23 @@
-import timeit
-
 CONDICAO_DE_SAIDA = 0
 
-def le_cartas():
-    cartas_lidas = list()
-    try:
-        while True:
-            carta = int(input())
-            if carta == CONDICAO_DE_SAIDA:
-                break
-            cartas_lidas.append(carta)
-            cartas_descartadas, carta_restante = remove_cartas(cartas_lidas.copy())
-            imprime_resultado(cartas_descartadas, carta_restante)
-    except EOFError:
-        pass
-
-def remove_cartas(cartas_lidas):
-    cartas_descartadas = list()
-    if len(cartas_lidas) >= 2:
-        while len(cartas_lidas) > 1:
-            cartas_descartadas.append(cartas_lidas.pop(0))
-            cartas_lidas.append(cartas_lidas.pop(0))
-
-    return cartas_descartadas, cartas_lidas.pop()
-
-
-def imprime_resultado(cartas_descartadas, carta_restante):
-
-    texto_cartas_descartadas = 'Discarded cards: {}'.format(', '.join(map(str, cartas_descartadas)))
-
-    print(texto_cartas_descartadas.strip())
-    print('Remaining card: {}'.format(carta_restante))
-
-
-if __name__ == '__main__':
-    le_cartas()
+numero_lido = -1
+while numero_lido != CONDICAO_DE_SAIDA:
+    numero_lido = int(input())
+    lista_numeros = [numero for numero in range(1, numero_lido + 1)]
+    indice = 0
+    numeros_descartados = list()
+    texto_cartas_descartadas = 'Discarded cards:'
+    while indice < len(lista_numeros) - 1:
+        numero_descartado = lista_numeros[0]
+        numeros_descartados.append(numero_descartado)
+        numero_movido = lista_numeros[1]
+        lista_numeros.remove(lista_numeros[0])
+        lista_numeros.remove(lista_numeros[0])
+        lista_numeros.append(numero_movido)
+        if texto_cartas_descartadas == 'Discarded cards:':
+            texto_cartas_descartadas = texto_cartas_descartadas + ' ' + str(numero_descartado)
+        else:
+            texto_cartas_descartadas = texto_cartas_descartadas + ', ' + str(numero_descartado)
+    if len(lista_numeros) > 0:
+        print(texto_cartas_descartadas.strip())
+        print('Remaining card: {}'.format(lista_numeros[0]))
