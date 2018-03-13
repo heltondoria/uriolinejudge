e1ec952c56b631636033e343f063fb9c118db2db
Helton Dória Costa <helton.doria@outlook.com>
2018-03-13 01:33:50 -0300

    versão mais curta e mais lenta do 1864 - passando

--- quarta_e_quinta_lista/1864.py
+++ quarta_e_quinta_lista/1864.py
@@ -1,7 +1,3 @@
 frase = "LIFE IS NOT A PROBLEM TO BE SOLVED"
-
 quantidade_letras = int(input())
-texto = ""
-for i in range(quantidade_letras):
-    texto = "{}{}".format(texto, frase[i])
-print(texto)
+print(frase[:quantidade_letras])
