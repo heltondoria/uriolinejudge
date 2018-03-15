8984e225731d4e86e18db6877929f6600ac153ca
Helton Dória Costa <helton.doria@outlook.com>
2018-03-15 00:18:03 -0300

    1435 passando

--- quarta_e_quinta_lista/1435.py
+++ quarta_e_quinta_lista/1435.py
@@ -1,6 +1,14 @@
-def get_identity_square_matrix(size):
-    matrix = [[1 for column_index in range(size)] for row_index in range(size)]
-    return matrix
+def print_matriz(matrix, size):
+    width = 3
+
+    for x in range(size):
+        for i, num in enumerate(matrix[x]):
+            if i == size - 1:
+                print('{0:{width}}'.format(num, width=width), end='')
+            else:
+                print('{0:{width}}'.format(num, width=width), end=' ')
+        print()
+    print('')
 
 
 while True:
@@ -8,14 +16,12 @@ while True:
     if tamanho == 0:
         break
 
-    matriz = get_identity_square_matrix(tamanho)
+    matriz = [[1 for i in range(tamanho)] for j in range(tamanho)]
     linha = " "
     for indice_linha in range(len(matriz)):
         for indice_coluna in range(len(matriz[indice_linha])):
             x = min(indice_linha + 1, tamanho - indice_linha)
             y = min(indice_coluna + 1, tamanho - indice_coluna)
             matriz[indice_linha][indice_coluna] = min(x, y)
-            elemento = "{}  ".format(str(matriz[indice_linha][indice_coluna]))
-            linha = "{} {}".format(linha, elemento)
-        linha = "{}{}".format(linha.rstrip(), '\n ')
-    print("{}".format(linha.rstrip(' ')))
+
+    print_matriz(matriz, tamanho)
